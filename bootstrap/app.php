d8ff650a7360c52f98d85f972801ebbb8a8fb378
NazrilFK <nazrilfk@gmail.com>
2026-08-13 00:29:44 +0700

    Fix HTTPS and proxy configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->statefulApi();
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->append(
             \Illuminate\Http\Middleware\HandleCors::class
         );
