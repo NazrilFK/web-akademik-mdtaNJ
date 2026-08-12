8ca2f235ad7f20a8d55e6b8dfadc6227dfdbaa6a
NazrilFK <nazrilfk@gmail.com>
2026-08-13 00:00:49 +0700

    Fix HTTPS and proxy configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Auth\Events\Login;
 use Carbon\Carbon;
 
@@ -16,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Carbon::setLocale('id');    
+        Carbon::setLocale('id');
+
+        URL::forceScheme('https');
 
         Event::listen(Login::class, function (Login $event) {
             $event->user->update([
